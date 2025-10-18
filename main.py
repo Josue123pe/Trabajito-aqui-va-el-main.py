cdd8839114a0669a0799e3323ae8b49c48cbdeda
Josue123pe <160342395+Josue123pe@users.noreply.github.com>
2025-10-17 21:22:49 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
-from http.server import SimpleHTTPRequestHandler, HTTPServer
+from flask import Flask, request
+from flask_cors import CORS
+app = Flask(__name__)
+CORS(app)
 
-# Configura la dirección y el puerto
-host = "localhost"
-port = 8000
 
-# Inicia un servidor HTTP simple
-server = HTTPServer((host, port), SimpleHTTPRequestHandler)
-print(f"Servidor corriendo en http://{host}:{port}")
-print("Presiona Ctrl + C para detenerlo")
+@app.route("/", methods=[ "POST"])
+def hola_mundo():
+    import pusher
+    data = request.get_json()
+   
+    pusher_client = pusher.Pusher(
+        app_id = '2065491',
+        key = "08f9ca3827443d276de3",
+        secret = "63d6cd6ed91c56e3521d",
+        cluster = "mt1",
+        ssl=True
+    )
 
-try:
-    server.serve_forever()
-except KeyboardInterrupt:
-    print("\nServidor detenido.")
-    server.server_close()
+    if data and "message" in data:
+        message = data["message"]
+    else:
+        message = request.form.get("message") or request.get_data(as_text=True) or ""
+
+    pusher_client.trigger('my-channel', 'my-event', message)
+    return ".."
+
+if __name__ == "__main__":
+    app.run(debug=True)
